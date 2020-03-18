73c69eec1e9f2e6594d0789ad47b53b375b686bf
Sajith Konara <konara.sajith@gmail.com>
2020-03-18 11:38:13 +0530

    Initial Commit

---
+++ Corona/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Corona
+//
+//  Created by Sajith Konara on 3/18/20.
+//  Copyright © 2020 Sajith Konara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
